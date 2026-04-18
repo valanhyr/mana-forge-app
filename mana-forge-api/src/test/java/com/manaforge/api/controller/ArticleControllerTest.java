7db22f0a1b6b32b16d68bc5a101ce401078df87e
valanhyr <valanhyr@outlook.es>
2026-04-18 10:55:48 +0200

    wip: test

--- mana-forge-api/src/test/java/com/manaforge/api/controller/ArticleControllerTest.java
+++ mana-forge-api/src/test/java/com/manaforge/api/controller/ArticleControllerTest.java
@@ -7,7 +7,7 @@ import com.manaforge.api.service.ArticleService;
 import com.manaforge.api.service.OAuth2LoginSuccessHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
--- mana-forge-api/src/test/java/com/manaforge/api/controller/collection/FooterLegalControllerTest.java
+++ mana-forge-api/src/test/java/com/manaforge/api/controller/collection/FooterLegalControllerTest.java
@@ -8,7 +8,7 @@ import com.manaforge.api.service.OAuth2LoginSuccessHandler;
 import com.manaforge.api.service.StrapiService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
